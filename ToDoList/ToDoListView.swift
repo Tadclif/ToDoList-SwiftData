8fbce568e402d2b6f10c5bf937830e0a5cfd4bb9
Tadclif <cliftont@bc.edu>
2024-10-16 13:40:16 -0400

    second commit

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift", "Build Apps", "Change the World", "Bring the Awesome", "Take a Vacation"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach(toDos, id: \.self) { toDo in
+                    NavigationLink{
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
+                }
+            }
+            .navigationTitle("To Do list")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
         }
-        .padding()
+        
     }
 }
 
